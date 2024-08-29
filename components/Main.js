9f588049a82a9d7de9c57092a7edcc39837f87d2
LeonardoMeyer <leonardo.meyerr@outlook.com>
2024-08-29 17:39:51 -0300

    get accounts from api

--- components/Main.js
+++ components/Main.js
@@ -1,28 +1,68 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import CardAccount2 from './CardAccount2';
+import { useEffect, useState } from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+import CardAccount from './cardAccount'
+//import CardAccount2 from './CardAccount2'
+//import Calc from './Calc'
 
-export default function Content() {
-  const accounts = [
-    {
-      id: 1,
-      service: 'LinkedIn',
-      imgUrl: 'https://static.vecteezy.com/system/resources/previews/023/986/970/original/linkedin-logo-linkedin-logo-transparent-linkedin-icon-transparent-free-free-png.png',
-      userName: '@Leonardo Meyer'
-    },
-    {
-      id: 2,
-      service: 'GitHub',
-      imgUrl: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
-      userName: 'LeonardoMeyer'
-    },
-    {
-      id: 3,
-      service: 'Medium',
-      imgUrl: 'https://www.svgrepo.com/show/354057/medium-icon.svg',
-      userName: 'LeonardoMeyer'
-    }
-  ];
+export default function Content(){
+
+  const [accounts, setAccounts] = useState([])
+ 
+   useEffect(() => {
+        const getAccounts = async () => {
+            const response = await fetch('http://localhost:3000/account/list')
+            if(response.ok){
+              const data = await response.json()
+              console.log(data)
+              setAccounts(data.accounts)
+              return
+            }
+            console.log('Erro ao carregar accounts')
+            return
+        }
+
+        getAccounts()
+   }, [])
+
+
+    return (
+        <View style={styles.content}>
+        
+        {/* <Calc /> */}
+
+        { accounts.length === 0 && <Text>Loading...</Text>}
+
+        {
+          accounts.map( (account) => 
+            <CardAccount
+              key={account.id} 
+              service={account.service}
+              imgUrl={account.logo_image}
+              userName={account.username}
+            /> 
+          )
+        }
+        </View>
+    )
+}
+  //     id: 1,
+  //     service: 'LinkedIn',
+  //     imgUrl: 'https://static.vecteezy.com/system/resources/previews/023/986/970/original/linkedin-logo-linkedin-logo-transparent-linkedin-icon-transparent-free-free-png.png',
+  //     userName: '@Leonardo Meyer'
+  //   },
+  //   {
+  //     id: 2,
+  //     service: 'GitHub',
+  //     imgUrl: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+  //     userName: 'LeonardoMeyer'
+  //   },
+  //   {
+  //     id: 3,
+  //     service: 'Medium',
+  //     imgUrl: 'https://www.svgrepo.com/show/354057/medium-icon.svg',
+  //     userName: 'LeonardoMeyer'
+  //   }
+  // ];
 
   return (
     <View style={styles.content}>
@@ -36,7 +76,6 @@ export default function Content() {
       ))}
     </View>
   );
-}
 
 const styles = StyleSheet.create({
   content: {
---
+++ components/estudo-objeto.js
@@ -0,0 +1,31 @@
+// const props = {
+//     nome: 'Renan',
+//     idade: 40,
+//     soma: (v1, v2) => { return v1 + v2; },
+//     end: {
+//       rua: 'Dos Bobos',
+//       numero: 0,
+//     },
+//     itensCompra: ["PC", "Monitor", "Placa Mãe"]
+//   };
+//   const props2 = {}
+  
+
+// itensCompra: ["pc", "monitor", "placa-mae"]
+
+// //adiciona uma nova propiedade
+// props.teste = "ola"
+// //deleta uma propiedade
+// delete props.teste
+
+// const idade = props.idade
+// const nome = props.nome
+// //destruct - desestruturaçao
+// const {nome, idade} = props
+
+// const [ic1, ic2]= props2.itensCompra
+
+// //spread = espalhamento
+// const propsTudo = {...props, idade: 50}
+
+// console.log(props.["outros itens"])
